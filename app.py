dce1a76332e06faba6d733a3b7178f01096e23b4
wadeking98 <wkingnumber2@gmail.com>
2024-08-22 11:41:47 -0700

    added v param
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- app.py
+++ app.py
@@ -12,9 +12,10 @@ def hello():
     d = request.args.get('d')
     m = request.args.get('m')
     n = request.args.get('n')
-    if None in [s,b,d,m,n]:
-        return f'Missing parameters {[s,b,d,m,n]}'
-    new_ciphertext = f"&b={b}&d={d}&m={m}&n={n}&s={s}"
+    n = request.args.get('v')
+    if None in [s,b,d,m,n,v]:
+        return f'Missing parameters {[s,b,d,m,n,v]}'
+    new_ciphertext = f"&v={v}&b={b}&d={d}&m={m}&n={n}&s={s}"
     new_ct_blocks = (len(new_ciphertext) // 8) + 1
     output = subprocess.run(["./rustpad", "web", "--oracle", "https://click.e.entaingroup.com/?qs=CTEXT", "-D", "0000000000000000", "-E", new_ciphertext, "-B", str(8), "--no-iv", "-t", str(100)], capture_output=True)
     return output.stdout
